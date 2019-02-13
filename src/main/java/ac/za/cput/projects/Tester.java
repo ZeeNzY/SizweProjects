21dd325199244477da9da135652ff9b5368e8719
Sizwe Ncikana <sizwe.ncikana@gmail.com>
2019-02-13 14:20:04 +0200

    commiting

---
+++ src/main/java/ac/za/cput/projects/Tester.java
@@ -0,0 +1,21 @@
+package ac.za.cput.projects;
+
+public class Tester {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int multiply(int x, int y){
+        return x * y;
+    }
+
+    public int subtract(int c, int d){
+        return c - d;
+    }
+
+    public int divide(int a, int y){
+        return a / y;
+    }
+
+}
